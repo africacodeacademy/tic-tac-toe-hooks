6b95bbfbb528af4e8cfb8fc9784bfd025829a1c0
Bonnie Schulkin <biz@bonnieschulkin.com>
2021-06-08 14:06:40 -0700

    Lift state to Game and add 'history' state

--- src/index.js
+++ src/index.js
@@ -10,41 +10,13 @@ function Square({ value, onClick }) {
   );
 }
 
-function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
-
-  function handleClick(i) {
-    if (calculateWinner(squares) || squares[i]) {
-      // return early if there's a winner or the square is already filled
-      return;
-    }
-    setSquares((previousValue) => {
-      const newValue = [...previousValue];
-
-      // determine value based on whose turn it is
-      newValue[i] = xIsNext ? "X" : "O";
-      return newValue;
-    });
-
-    setXIsNext((previousValue) => !previousValue);
-  }
-
+function Board({ squares, handleClick }) {
   function renderSquare(i) {
     return <Square value={squares[i]} onClick={() => handleClick(i)} />;
   }
 
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = "Winner: " + winner;
-  } else {
-    status = "Next player: " + (xIsNext ? "X" : "O");
-  }
-
   return (
     <div>
-      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -65,13 +37,39 @@ function Board() {
 }
 
 function Game() {
+  const [history, setHistory] = useState([{ squares: Array(9).fill(null) }]);
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const current = history[history.length - 1];
+  const winner = calculateWinner(current.squares);
+  const status = winner
+    ? "Winner: " + winner
+    : "Next player: " + (xIsNext ? "X" : "O");
+
+  function handleClick(i) {
+    if (calculateWinner(current.squares) || current.squares[i]) {
+      // return early if there's a winner or the square is already filled
+      return;
+    }
+
+    // update the square that was clicked
+    const newSquares = [...current.squares];
+    newSquares[i] = xIsNext ? "X" : "O";
+
+    // add this board state the history
+    setHistory((prevHistory) => prevHistory.concat([{ squares: newSquares }]));
+
+    // update which user is "up"
+    setXIsNext((previousValue) => !previousValue);
+  }
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board />
+        <Board squares={current.squares} handleClick={handleClick} />
       </div>
       <div className="game-info">
-        <div>{/* status */}</div>
+        <div>{status}</div>
         <ol>{/* TODO */}</ol>
       </div>
     </div>
